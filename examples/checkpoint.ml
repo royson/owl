eae7263099d04158604e94444d31facc0a588283
Royson Lee <rs@roysonlee.com>
2018-03-04 11:46:54 +0000

    Playing with plot functionality.

--- examples/checkpoint.ml
+++ examples/checkpoint.ml
@@ -9,11 +9,27 @@ open Algodiff.S
 
 let make_network input_shape =
   input input_shape
-  |> lambda (fun x -> Maths.(x / F 256.))
+(*   |> lambda (fun x -> Maths.(x / F 256.))
   |> conv2d [|5;5;1;32|] [|1;1|] ~act_typ:Activation.Relu
   |> max_pool2d [|2;2|] [|2;2|]
   |> dropout 0.1
   |> fully_connected 1024 ~act_typ:Activation.Relu
+  |> linear 10 ~act_typ:Activation.Softmax
+  |> get_network
+ *)
+    (* |> conv2d [|3;3;1;32|] [|1;1|] ~act_typ:Activation.Relu
+    |> conv2d [|3;3;32;64|] [|1;1|] ~act_typ:Activation.Relu
+    |> max_pool2d [|2;2|] [|2;2|]
+    |> dropout 0.5
+    |> flatten
+    |> linear 128 ~act_typ:Activation.Relu
+    |> normalisation
+    |> dropout 0.5
+    |> linear 10 ~act_typ:Activation.Softmax
+    |> get_network *)
+  |> flatten 
+  |> linear 256 ~act_typ:Activation.Tanh
+  |> linear 128 ~act_typ:Activation.Relu
   |> linear 10 ~act_typ:Activation.Softmax
   |> get_network
 
@@ -25,16 +41,38 @@ let train () =
   (* define checkpoint function *)
   let chkpt state =
     let open Checkpoint in
-    if state.current_batch mod 10 = 0 then (
-      Owl_log.warn "this is a customised checkpoint ...";
-      state.stop <- true
+    if state.current_batch mod (state.batches - 1) = 0 then (
+      Owl_log.info "Plotting loss function..";   
+      let z = Array.map unpack_flt state.loss in
+      let c = Array.sub z 0 state.batches in 
+      (* Array.map (Owl_log.info "%.6f") c; *)
+
+      let h = Plot.create "seq_mnist_MLP_Adagrad_0.05.png" in
+      let f x = c.(Maths.(pack_flt x - F 1.) |> unpack_flt |> int_of_float) in
+      let x_range = Maths.(F (float_of_int (Array.length c)) - F 1.) in
+      let y_range arr = Array.fold_left Pervasives.max arr.(0) arr |> Pervasives.(+.) in
+      Plot.set_foreground_color h 0 0 0;
+      Plot.set_background_color h 255 255 255;
+      Plot.set_title h "Sequential MNist Adam 0.01";
+      Plot.set_xrange h 0. (unpack_flt Maths.(x_range + F 10.));
+      Plot.set_yrange h 0. (y_range c 2.0);
+      Plot.set_xlabel h "Batch";
+      Plot.set_ylabel h "Loss";
+      Plot.set_font_size h 8.;
+      Plot.set_pen_size h 3.;
+
+      Plot.plot_fun ~h f 1. (unpack_flt x_range);
+
+      Plot.output h;
+
+      (* state.stop <- true; *)
     )
   in
 
   (* plug in chkpt into params *)
   let params = Params.config
-    ~batch:(Batch.Sample 100) ~learning_rate:(Learning_Rate.Adagrad 0.005)
-    ~checkpoint:(Checkpoint.Custom chkpt) ~stopping:(Stopping.Const 1e-6) 10.
+    ~batch:(Batch.Sample 100) ~learning_rate:(Learning_Rate.Adagrad 0.05)
+    ~checkpoint:(Checkpoint.Custom chkpt) ~stopping:(Stopping.Const 1e-6) 0.5
   in
   (* keep restarting the optimisation until it finishes *)
   let state = Graph.train ~params network x y in
