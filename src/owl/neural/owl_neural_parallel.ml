0162c2c9ef9b9001eaa3b779b5e3d53d59a5d166
Royson Lee <rs@roysonlee.com>
2018-05-16 14:09:21 +0100

    speed up bug.

--- src/owl/neural/owl_neural_parallel.ml
+++ src/owl/neural/owl_neural_parallel.ml
@@ -478,8 +478,8 @@ module Make (M : ModelSig) (E : EngineSig) = struct
       E.set (string_of_int task.sid ^ "finish") Checkpoint.(state.stop); 
 
       let current_progression = E.progressive_num () in
-      (* Add/Remove workers for PASP barrier every 100 iterations *)
-      let workers_changed = match Checkpoint.(state.current_batch mod 100 = 0) with
+      (* Add/Remove workers for PASP barrier every 5 iterations *)
+      let workers_changed = match Checkpoint.(state.current_batch mod 5 = 0) with
         | false -> false
         | true  -> let b  = Owl_stats.uniform_int_rvs ~a:0 ~b:1 in
                    let cw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
