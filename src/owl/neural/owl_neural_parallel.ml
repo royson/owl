0f43b17ba49f3357b0bc99c8f3eed4b3908726bc
Royson Lee <rs@roysonlee.com>
2018-05-23 17:18:47 +0100

    Simulate more workers joining than leaving.

--- src/owl/neural/owl_neural_parallel.ml
+++ src/owl/neural/owl_neural_parallel.ml
@@ -490,12 +490,13 @@ module Make (M : ModelSig) (E : EngineSig) = struct
         (* | true  -> E.add_workers current_progression *)
         (* Capricious mode *)
         | true  -> let b  = Owl_stats.uniform_int_rvs ~a:0 ~b:1 in
-                   let cw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
+                   let aw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
+                   let lw = Owl_stats.uniform_int_rvs ~a:1 ~b:2 in
                    match b with
-                   | 1 -> Owl_log.debug "%i workers attempting to join." cw;
-                          E.add_workers cw
-                   | _ -> Owl_log.debug "%i workers attempting to leave." cw;
-                          E.remove_workers cw
+                   | 1 -> Owl_log.debug "%i workers attempting to join." aw;
+                          E.add_workers aw
+                   | _ -> Owl_log.debug "%i workers attempting to leave." lw;
+                          E.remove_workers lw
          
       in
 
