a9d029defcc88bf118237969921bb485afc7da34
Royson Lee <rs@roysonlee.com>
2018-06-03 23:07:16 +0100

    Join more.

--- src/owl/neural/owl_neural_parallel.ml
+++ src/owl/neural/owl_neural_parallel.ml
@@ -479,18 +479,17 @@ module Make (M : ModelSig) (E : EngineSig) = struct
       (* let workers_changed = match Checkpoint.(state.current_batch mod (state.batches_per_epoch * 5) = 0) with *)
       let workers_changed = match Checkpoint.(state.current_batch mod 100 = 0) with
         | false -> false
+        | true  -> let b  = Owl_stats.uniform_int_rvs ~a:0 ~b:10 in
+                   let aw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
+                   let lw = Owl_stats.uniform_int_rvs ~a:1 ~b:1 in
+                   match b with
+                   | 3 -> Owl_log.debug "%i workers attempting to leave." 1;
+                          E.remove_workers 1
+                   | _ -> Owl_log.debug "%i workers attempting to join." aw;
+                          E.add_workers aw
         (* Progressive mode *)
         (* | true  -> E.add_workers current_progression *)
         (* Dynamic mode *)
-        | true  -> let b  = Owl_stats.uniform_int_rvs ~a:0 ~b:7 in
-                   let aw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
-                   let lw = Owl_stats.uniform_int_rvs ~a:1 ~b:2 in
-                   match b with
-                   | 3 -> Owl_log.debug "%i workers attempting to leave." lw;
-                          E.remove_workers lw
-                   | _ -> Owl_log.debug "%i workers attempting to join." aw;
-                          E.add_workers aw
-         
       in
 
       (* Detect if workers changed *)
