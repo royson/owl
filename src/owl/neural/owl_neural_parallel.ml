e938e90eb99ceb0496f5a0831f2963f2598abc4d
Royson Lee <rs@roysonlee.com>
2018-06-05 16:28:37 +0100

    Rate of workers more.

--- src/owl/neural/owl_neural_parallel.ml
+++ src/owl/neural/owl_neural_parallel.ml
@@ -481,17 +481,17 @@ module Make (M : ModelSig) (E : EngineSig) = struct
       let workers_changed = match Checkpoint.(state.current_batch mod 100 = 0) with
         | false -> false
         (* Progressive mode *)
-        | true  -> E.add_workers current_progression
+        (* | true  -> E.add_workers current_progression *)
         (* Dynamic mode *)
-(*         | true  -> let b  = Owl_stats.uniform_int_rvs ~a:0 ~b:2 in
+        | true  -> let b  = Owl_stats.uniform_int_rvs ~a:0 ~b:10 in
                    let aw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
                    let lw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
                    match b with
-                   | 1 -> Owl_log.debug "%i workers attempting to join." aw;
+                   | 9 -> Owl_log.debug "%i workers attempting to leave." 1;
+                          E.remove_workers 1
+                   | _ -> Owl_log.debug "%i workers attempting to join." aw;
                           E.add_workers aw
-                   | _ -> Owl_log.debug "%i workers attempting to leave." lw;
-                          E.remove_workers lw
- *)         
+         
       in
 
       (* Detect if workers changed *)
