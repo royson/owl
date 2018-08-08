a7cbb3ba93cfbc695c561a31285a679af2f4ff12
Royson Lee <rs@roysonlee.com>
2018-08-08 23:08:14 +0100

    AA test.

--- src/owl/neural/owl_neural_parallel.ml
+++ src/owl/neural/owl_neural_parallel.ml
@@ -488,16 +488,16 @@ module Make (M : ModelSig) (E : EngineSig) = struct
       (* let workers_changed = match Checkpoint.(state.current_batch mod 100 = 0) with *)
         | false -> false
         (* Progressive mode *)
-        (* | true  -> E.add_workers current_progression *)
+        | true  -> E.add_workers current_progression
       (*  Dynamic mode *)
-        | true  -> let b  = Owl_stats.uniform_int_rvs ~a:0 ~b:1 in
+(*         | true  -> let b  = Owl_stats.uniform_int_rvs ~a:0 ~b:1 in
                    let aw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
                    let lw = Owl_stats.uniform_int_rvs ~a:1 ~b:8 in
                    match b with
                    | 1 -> Owl_log.debug "%i workers attempting to join." aw;
                           E.add_workers aw
                    | _ -> Owl_log.debug "%i workers attempting to leave." lw;
-                          E.remove_workers lw
+                          E.remove_workers lw *)
          
       in
 
